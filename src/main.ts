a1a839d54daf847c070e6898f573299a84272c71
Hudson Lima Tavares <109999011+hltav@users.noreply.github.com>
2025-07-12 18:13:40 -0300

    Main.Ts Update

---
+++ src/main.ts
@@ -0,0 +1,52 @@
+import 'reflect-metadata';
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { join } from 'path';
+import multipart from '@fastify/multipart';
+import fastifyStatic from '@fastify/static';
+import cors from '@fastify/cors';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+
+  await app.register(cors, {
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
+
+  await app.register(multipart, {
+    limits: {
+      fileSize: 3 * 1024 * 1024, // 3 MB
+    },
+  });
+
+  await app.register(fastifyStatic, {
+    root: join(process.cwd(), 'uploads'),
+    prefix: '/uploads/',
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+
+  await app.listen({ port: 3003, host: '0.0.0.0' });
+}
+bootstrap().catch((err) => {
+  console.error('Erro ao iniciar aplicação:', err);
+  process.exit(1);
+});
