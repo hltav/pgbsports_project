8762be98b16229546f1be9b262bba6937560145d
Hudson Lima Tavares <109999011+hltav@users.noreply.github.com>
2025-07-12 19:15:42 -0300

    Changed ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,5 +1,3 @@
-require('dotenv').config();
-
 module.exports = {
   apps: [
     {
@@ -7,19 +5,19 @@ module.exports = {
       script: "dist/main.js",
       env: {
         NODE_ENV: "production",
-        DATABASE_URL: process.env.DATABASE_URL,
-        JWT_SECRET: process.env.JWT_SECRET,
-        JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN,
-        MAIL_SERVICE_API: process.env.MAIL_SERVICE_API,
-        CACHE_TTL_SECONDS: process.env.CACHE_TTL_SECONDS,
-        SPORTRADAR_API_KEY: process.env.SPORTRADAR_API_KEY,
-        APISPORTS_API_KEY: process.env.APISPORTS_API_KEY,
-        MAIL_HOST: process.env.MAIL_HOST,
-        MAIL_PORT: process.env.MAIL_PORT,
-        MAIL_USER: process.env.MAIL_USER,
-        MAIL_PASS: process.env.MAIL_PASS,
-        MAIL_FROM: process.env.MAIL_FROM,
-        FRONTEND_URL: process.env.FRONTEND_URL
+        DATABASE_URL: "DATABASE_URL",
+        JWT_SECRET: "JWT_SECRET",
+        JWT_EXPIRES_IN: "JWT_EXPIRES_IN",
+        MAIL_SERVICE_API: "MAIL_SERVICE_API",
+        CACHE_TTL_SECONDS: "CACHE_TTL_SECONDS",
+        SPORTRADAR_API_KEY: "SPORTRADAR_API_KEY",
+        APISPORTS_API_KEY: "APISPORTS_API_KEY",
+        MAIL_HOST: "MAIL_HOST",
+        MAIL_PORT: "MAIL_PORT",
+        MAIL_USER: "MAIL_USER",
+        MAIL_PASS: "MAIL_PASS",
+        MAIL_FROM: "MAIL_FROM",
+        FRONTEND_URL: "FRONTEND_URL"
       }
     }
   ]
