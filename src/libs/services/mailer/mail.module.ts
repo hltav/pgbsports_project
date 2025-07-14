4e40d2c0b20933d1c209377e37079812cc867f85
Hudson Lima Tavares <109999011+hltav@users.noreply.github.com>
2025-07-14 15:37:44 -0300

    Update mail.module.ts

--- src/libs/services/mailer/mail.module.ts
+++ src/libs/services/mailer/mail.module.ts
@@ -1,37 +1,40 @@
-import { Module } from '@nestjs/common';
-import { MailerModule } from '@nestjs-modules/mailer';
-import { ConfigModule, ConfigService } from '@nestjs/config'; // Importe ConfigModule e ConfigService
-import { EmailService } from './mail.service';
-import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
-import * as path from 'path';
-
 @Module({
   imports: [
     ConfigModule,
     MailerModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        transport: {
-          host: configService.get<string>('MAIL_HOST'),
-          port: configService.get<number>('MAIL_PORT'),
-          secure: false,
-          auth: {
-            user: configService.get<string>('MAIL_USER'),
-            pass: configService.get<string>('MAIL_PASS'),
+      useFactory: (configService: ConfigService) => {
+        // Validação das variáveis de ambiente
+        const requiredEnvVars = ['MAIL_HOST', 'MAIL_USER', 'MAIL_PASS', 'MAIL_FROM'];
+        requiredEnvVars.forEach(envVar => {
+          if (!configService.get(envVar)) {
+            throw new Error(`Variável de ambiente ${envVar} não definida`);
+          }
+        });
+
+        return {
+          transport: {
+            host: configService.get<string>('MAIL_HOST'),
+            port: configService.get<number>('MAIL_PORT', 587),
+            secure: configService.get<boolean>('MAIL_SECURE', false),
+            auth: {
+              user: configService.get<string>('MAIL_USER'),
+              pass: configService.get<string>('MAIL_PASS'),
+            },
           },
-        },
-        defaults: {
-          from: configService.get<string>('MAIL_FROM'),
-        },
-        template: {
-          dir: path.resolve(__dirname, 'templates'),
-          adapter: new HandlebarsAdapter(),
-          options: {
-            strict: true,
+          defaults: {
+            from: `"${configService.get<string>('MAIL_FROM_NAME', 'RT Sports')}" <${configService.get<string>('MAIL_FROM')}>`,
           },
-        },
-      }),
+          template: {
+            dir: '/var/www/rtsmanager_backend/dist/libs/services/mailer/templates', 
+            adapter: new HandlebarsAdapter(),
+            options: {
+              strict: true,
+            },
+          },
+        };
+      },
     }),
   ],
   providers: [EmailService],
