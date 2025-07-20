dfd9a097a526cf0a8145712115559c1cfcf64d05
Hudson Lima Tavares <109999011+hltav@users.noreply.github.com>
2025-07-20 11:37:12 -0300

    Changed AuthController User

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -66,7 +66,10 @@ export class AuthController {
     return res.send({
       accessToken,
       refreshToken,
-      user,
+      user: {
+        id: user.id,
+        email: user.email,
+      },
     });
   }
 
