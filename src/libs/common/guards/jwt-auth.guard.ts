0ce486c74fff416ef08f0ef92336e7beb3442283
Hudson Lima Tavares <109999011+hltav@users.noreply.github.com>
2025-07-30 00:06:56 -0300

    AuthGuard Changed

--- src/libs/common/guards/jwt-auth.guard.ts
+++ src/libs/common/guards/jwt-auth.guard.ts
@@ -1,10 +1,53 @@
+// import { ExecutionContext, Injectable } from '@nestjs/common';
+// import { AuthGuard } from '@nestjs/passport';
+// import { Request } from './../../../libs/common/interface/request.interface';
+
+// @Injectable()
+// export class JwtAuthGuard extends AuthGuard('jwt') {
+//   async canActivate(context: ExecutionContext): Promise<boolean> {
+//     try {
+//       const isAuthorized = await super.canActivate(context);
+//       if (!isAuthorized) {
+//         return false;
+//       }
+
+//       const request = context.switchToHttp().getRequest<Request>();
+//       const user = request.user;
+
+//       if (user) {
+//         user.role = user.role || 'USER';
+//       }
+
+//       return !!user;
+//     } catch (error) {
+//       console.error('JwtAuthGuard - Erro:', error);
+//       throw error;
+//     }
+//   }
+// }
+
 import { ExecutionContext, Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { Reflector } from '@nestjs/core';
 import { Request } from './../../../libs/common/interface/request.interface';
-
+import { IS_PUBLIC_KEY } from '../decorator/public.decorator';
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
+  constructor(private reflector: Reflector) {
+    super();
+  }
+
   async canActivate(context: ExecutionContext): Promise<boolean> {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (isPublic) {
+      // rota pública, liberar acesso sem autenticação
+      return true;
+    }
+
     try {
       const isAuthorized = await super.canActivate(context);
       if (!isAuthorized) {
