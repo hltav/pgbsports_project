61060016784db4a30801bfc52127a29532e6404a
Hudson Lima Tavares <109999011+hltav@users.noreply.github.com>
2025-09-20 16:06:44 -0300

    Changed Main.ts

--- src/main.ts
+++ src/main.ts
@@ -45,7 +45,7 @@ async function bootstrap() {
     },
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
     preflightContinue: false,
   });
 
